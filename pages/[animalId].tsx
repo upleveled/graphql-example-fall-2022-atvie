3a37f076b6512b5c30b9e1a5cba0973238b208a6
Victor Ejike Nwosu <ejikello123@yahoo.com>
2022-11-30 18:04:12 +0100

    Add Delete functionality

--- pages/[animalId].tsx
+++ pages/[animalId].tsx
@@ -1,6 +1,9 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import { GetServerSidePropsContext } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { Animal as AnimalProps } from '../database/animals';
 import queryGraphql from '../shared/query-graphql';
 import styles from '../styles/Home.module.css';
@@ -9,7 +12,31 @@ type Props = {
   animal: AnimalProps;
 };
 
+const deleteAnimal = gql`
+  mutation DeleteAnimal($id: ID!) {
+    deleteAnimal(id: $id) {
+      id
+    }
+  }
+`;
+
 export default function Animal(props: Props) {
+  const router = useRouter();
+
+  const [deletedAnimal] = useMutation(deleteAnimal);
+
+  async function handleDelete(id: number) {
+    const animal = await deletedAnimal({
+      variables: {
+        id: id,
+      },
+    });
+
+    if (animal.data) {
+      await router.push('/');
+    }
+  }
+
   return (
     <>
       <header className={styles.header}>
@@ -29,6 +56,17 @@ export default function Animal(props: Props) {
             <h2>{props.animal.name}</h2>
             <p>{props.animal.type}</p>
             <p>{props.animal.accessory}</p>
+          </div>
+
+          <div>
+            <button>Edit</button>
+            <button
+              onClick={async () => {
+                await handleDelete(props.animal.id);
+              }}
+            >
+              Delete
+            </button>
           </div>
         </div>
       </div>
