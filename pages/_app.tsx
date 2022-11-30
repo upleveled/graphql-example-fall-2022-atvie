803e4d2c9b66f18d8ee82d95cb285b635ea2ff29
Victor Ejike Nwosu <ejikello123@yahoo.com>
2022-11-30 12:26:13 +0100

    Setup connection between apollo client and server

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
 import '../styles/globals.css';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client';
 import type { AppProps } from 'next/app';
 
+export const cache: InMemoryCache = new InMemoryCache({});
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  uri: `${baseUrl}/graphql`,
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
