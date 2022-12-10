d6428b28b759ac1b7f3bc692eb752179000a9524
Victor Ejike Nwosu <ejikello123@yahoo.com>
2022-12-10 01:56:05 +0100

    Add logout button to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,23 @@
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
-export default function Header() {
+type HeaderProps = { animal?: { name: string } };
+
+export default function Header(props: HeaderProps) {
   return (
     <header>
       <nav className={styles.header}>
         <div>
           <Link href="/">Home</Link>
           <Link href="/create-animal">Create Animal</Link>
-          <Link href="/animal-admin">Admin Animal</Link>
+          <Link href="/admin-animal">Admin Animal</Link>
         </div>
         <div>
-          <Link href="/login">Login</Link>
+          {props.animal?.name === 'Ralph' ? (
+            <Link href="/logout">Logout</Link>
+          ) : (
+            <Link href="/login">Login</Link>
+          )}
         </div>
       </nav>
     </header>
