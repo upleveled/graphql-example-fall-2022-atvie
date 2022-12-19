b0b64650c2d26c94d34093f9262367ce790e3387
Victor Ejike Nwosu <ejikello123@yahoo.com>
2022-12-20 00:10:58 +0100

    Make dynamic page in app component

---
+++ app/[animalId]/page.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import queryGraphql from '../../shared/query-graphql';
+import styles from '../../styles/Home.module.css';
+
+type Props = {
+  params: any;
+};
+
+export default async function Animal(props: Props) {
+  const animalId = props.params;
+
+  const { animal } = await queryGraphql(
+    `
+  query($animalId: ID!) {
+    animal(id: $animalId) {
+      id
+      accessory
+      name
+      type
+  }
+  }
+  `,
+    animalId,
+  );
+
+  return (
+    <>
+      <header className={styles.header}>
+        <Link href="/">← Back to Animals</Link>
+      </header>
+      <h1 className={styles.title}>{animal.name}</h1>
+      <div className={styles.grid}>
+        <div className={styles.gridItem}>
+          <Image
+            src={`/images/${animal.id}-${animal.name.toLowerCase()}.jpeg`}
+            alt={animal.name}
+            width={400}
+            height={400}
+          />
+          <div>
+            <h2>{animal.name}</h2>
+            <p>{animal.type}</p>
+            <p>{animal.accessory}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
