45db70561d2809f15ca5d1f7e056340f376a06c7
Victor Ejike Nwosu <ejikello123@yahoo.com>
2022-12-20 00:08:47 +0100

    Create head and layout for app component

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>GraphQL example fall 2022</title>
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import '../styles/globals.css';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client';
+import Header from '../components/Header';
+
+// import type { AppProps } from 'next/app';
+// import Layout from '../components/Layout';
+
+export const cache: InMemoryCache = new InMemoryCache({});
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  uri: `${baseUrl}/graphql`,
+  credentials: 'same-origin',
+});
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function Layout(props: LayoutProps) {
+  return (
+    <html lang="en">
+      <body>
+        <ApolloProvider client={client}>
+          <Header />
+          {props.children}
+        </ApolloProvider>
+      </body>
+    </html>
+  );
+}
